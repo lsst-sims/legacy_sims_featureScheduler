45655c671a7d08d3cc9655ea865a9b433cdc281d
Peter Yoachim <yoachim@uw.edu>
2017-08-08 15:27:28 -0700

    trying to make a marching army like survey

---
+++ examples/marching_test.py
@@ -0,0 +1,26 @@
+import numpy as np
+import lsst.sims.featureScheduler as fs
+from lsst.sims.featureScheduler.observatory import Speed_observatory
+import healpy as hp
+
+if __name__ == "__main__":
+
+    survey_length = 365  # days
+    # Define what we want the final visit ratio map to look like
+    target_map = fs.standard_goals()['r']
+
+    bfs = []
+    bfs.append(fs.Depth_percentile_basis_function())
+    bfs.append(fs.Target_map_basis_function(target_map=target_map))
+    bfs.append(fs.Quadrant_basis_function())
+    bfs.append(fs.Slewtime_basis_function())
+
+    weights = np.array([.5, 1., 1., 1.])
+    survey = fs.Simple_greedy_survey_fields(bfs, weights, block_size=10)
+    scheduler = fs.Core_scheduler([survey])
+
+    observatory = Speed_observatory()
+    observatory, scheduler, observations = fs.sim_runner(observatory, scheduler,
+                                                         survey_length=survey_length,
+                                                         filename='marching.db',
+                                                         delete_past=True)
