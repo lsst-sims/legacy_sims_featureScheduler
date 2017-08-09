2bc60bfdd6732de6c5552021637ab4289c935529
Peter Yoachim <yoachim@uw.edu>
2017-08-09 09:16:58 -0700

    got it to run and timed it out.

--- examples/marching_test.py
+++ examples/marching_test.py
@@ -16,11 +16,14 @@ if __name__ == "__main__":
     bfs.append(fs.Slewtime_basis_function())
 
     weights = np.array([.5, 1., 1., 1.])
-    survey = fs.Simple_greedy_survey_fields(bfs, weights, block_size=10)
+    survey = fs.Simple_greedy_survey_fields(bfs, weights, block_size=1)
     scheduler = fs.Core_scheduler([survey])
 
     observatory = Speed_observatory()
     observatory, scheduler, observations = fs.sim_runner(observatory, scheduler,
                                                          survey_length=survey_length,
-                                                         filename='marching.db',
+                                                         filename='marching_block_1.db',
                                                          delete_past=True)
+
+# block_size=10, surveylength of 365 had runtime of 163 min. and got 0.26e6 observations. So, 10 years would be 27 hours.
+# Going to block_size=1, runtime of 211 min, and 0.33e6 observations. So, 35 hours. Not too shabby! 
