5381e31fada6d68a61d657f893282f178c696a36
Peter Yoachim <yoachim@uw.edu>
2018-04-02 21:46:58 -0700

    flake8 and fix comcam example mask

--- examples/comcam_example.py
+++ examples/comcam_example.py
@@ -9,10 +9,16 @@ if __name__ == "__main__":
     # Need to crank up resolution for camcam
     nside = fs.set_default_nside(nside=256)
 
-    survey_length = 3.  # Days
+    survey_length = 20.  # Days
     target_map = fs.standard_goals(nside=nside)
+    ra, dec = fs.ra_dec_hp_map(nside=nside)
+    out_region = np.where((dec > np.radians(-40)) | (dec < np.radians(-50.)))
+    in_region = np.where((dec <= np.radians(-40)) | (dec >= np.radians(-50.)))
+    for key in target_map:
+        target_map[key][out_region] = 0.
+        target_map[key][in_region] = 1.
+
     years = np.round(survey_length/365.25)
-    target_map = fs.standard_goals(nside=nside)
     filters = ['g', 'r', 'i']
     surveys = []
 
@@ -21,14 +27,14 @@ if __name__ == "__main__":
         bfs.append(fs.M5_diff_basis_function(filtername=filtername, nside=nside))
         bfs.append(fs.Target_map_basis_function(filtername=filtername,
                                                 target_map=target_map[filtername],
-                                                out_of_bounds_val=hp.UNSEEN, nside=nside))
+                                                out_of_bounds_val=-10., nside=nside))
 
         bfs.append(fs.North_south_patch_basis_function(zenith_min_alt=50., nside=nside))
         #bfs.append(fs.Zenith_mask_basis_function(maxAlt=78., penalty=-100, nside=nside))
         bfs.append(fs.Slewtime_basis_function(filtername=filtername, nside=nside))
         bfs.append(fs.Strict_filter_basis_function(filtername=filtername))
 
-        weights = np.array([3.0, 0.3, 1., 3., 3.])
+        weights = np.array([3.0, 1.0, 1., 3., 3.])
         # Might want to try ignoring DD observations here, so the DD area gets covered normally
         surveys.append(fs.Greedy_survey_comcam(bfs, weights, block_size=1, filtername=filtername,
                                                dither=True, nside=nside))
@@ -48,5 +54,8 @@ if __name__ == "__main__":
                                                          delete_past=True)
 
 
+# To run maf:
+#  python run_glance.py comcam_baseline_0yrs.db comcam_test --camera ComCam
 
+# Time for 20 days:  205min = 3.5hr.  ~10min per day. 
 
