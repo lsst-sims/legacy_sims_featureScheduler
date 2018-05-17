b1dc998f239abff6b202a4a93a5091d1c823ad3a
Tiago <tribeiro@lsst.org>
2018-05-17 16:27:07 -0700

    disable cablewrap basis function off.

--- python/lsst/sims/featureScheduler/driver/config/config.py
+++ python/lsst/sims/featureScheduler/driver/config/config.py
@@ -104,11 +104,11 @@ for filtername in filters:
     bfs.append(fs.Avoid_Fast_Revists(filtername=filtername, gap_min=70., nside=nside))
     bfs.append(fs.Bulk_cloud_basis_function(max_cloud_map=cloud_map, nside=nside))
     bfs.append(fs.Moon_avoidance_basis_function(nside=nside, moon_distance=40.))
-    bfs.append(fs.CableWrap_unwrap_basis_function(nside=nside, activate_tol=70., unwrap_until=315,
-                                                  max_duration=90.))
+    # bfs.append(fs.CableWrap_unwrap_basis_function(nside=nside, activate_tol=70., unwrap_until=315,
+    #                                               max_duration=90.))
     # bfs.append(fs.NorthSouth_scan_basis_function(length=70.))
 
-    weights = np.array([2., 0.1, .1, 1., 3., 1.5, 1.0, 1.0, 1.0, 1.0])
+    weights = np.array([2., 0.1, .1, 1., 3., 1.5, 1.0, 1.0, 1.0])
     surveys.append(fs.Greedy_survey_fields(bfs, weights, block_size=1,
                                            filtername=filtername, dither=True,
                                            nside=nside,
