e53c7ee1ab596929c8c46062ba5b52a801978be7
Peter Yoachim <yoachim@uw.edu>
2019-01-28 20:47:38 -0800

    fix test typo

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,32 +11,31 @@ class TestFeatures(unittest.TestCase):
         """
         Test that the season utils work as intended
         """
+        night = 365.25 * 3.5
+        plain = season_calc(night)
+        assert(plain == 3)
+
+        mod2 = season_calc(night, modulo=2)
+        assert(mod2 == 1)
+
+        mod3 = season_calc(night, modulo=3)
+        assert(mod3 == 0)
+
+        mod3 = season_calc(night, modulo=3, max_season=2)
+        assert(mod3 == -1)
+
+        mod3 = season_calc(night, modulo=3, max_season=2, offset=-365.25*2)
+        assert(mod3 == 1)
+
+        mod3 = season_calc(night, modulo=3, max_season=2, offset=-365.25*10)
+        assert(mod3 == -1)
+
+        mod3 = season_calc(night, modulo=3, offset=-365.25*10)
+        assert(mod3 == -1)
 
 
 class TestMemory(lsst.utils.tests.MemoryTestCase):
-    night = 365.25 * 3.5
-    plain = season_calc(night)
-    assert(plain == 3)
-
-    mod2 = season_calc(night, modulo=2)
-    assert(mod2 == 1)
-
-    mod3 = season_calc(night, modulo=3)
-    assert(mod3 == 0)
-
-    mod3 = season_calc(night, modulo=3, max_season=2)
-    assert(mod3 == -1)
-
-    mod3 = season_calc(night, modulo=3, max_season=2, offset=-365.25*2)
-    assert(mod3 == 1)
-
-    mod3 = season_calc(night, modulo=3, max_season=2, offset=-365.25*10)
-    assert(mod3 == -1)
-
-    mod3 = season_calc(night, modulo=3, offset=-365.25*10)
-    assert(mod3 == -1)
-
-    
+    pass
 
 
 def setup_module(module):
