7ed5ad14dec1920c5367fcdc44b13be969d2a079
Peter Yoachim <yoachim@uw.edu>
2020-06-17 22:14:37 -0700

    fix rolling sections

--- python/lsst/sims/featureScheduler/basis_functions/rolling_funcs.py
+++ python/lsst/sims/featureScheduler/basis_functions/rolling_funcs.py
@@ -216,12 +216,12 @@ class Footprint_rolling_basis_function(Base_basis_function):
         for season in np.unique(seasons[self.rolling_footprint_indx]):
             if season >= 0:
                 self.coverage_trackers[season].update_conditions(conditions)
-                norm_coverage = self.coverage_trackers[season].feature/np.max(self.coverage_trackers[season].feature)
-                norm_footprint = self.footprints[season] * norm_coverage
+            norm_coverage = self.coverage_trackers[season].feature/np.max(self.coverage_trackers[season].feature)
+            norm_footprint = self.footprints[season] * norm_coverage
 
-                season_indx = np.where(seasons[self.rolling_footprint_indx] == season)[0]
-                desired = norm_footprint[self.rolling_footprint_indx[season_indx]] / self.all_rolling_sum * np.sum(self.survey_features['N_obs_all_%i' % season].feature[self.rolling_footprint_indx])
-                result[self.rolling_footprint_indx[season_indx]] = desired - self.survey_features['N_obs_%i' % season].feature[self.rolling_footprint_indx][season_indx]
+            season_indx = np.where(seasons[self.rolling_footprint_indx] == season)[0]
+            desired = norm_footprint[self.rolling_footprint_indx[season_indx]] / self.all_rolling_sum * np.sum(self.survey_features['N_obs_all_%i' % season].feature[self.rolling_footprint_indx])
+            result[self.rolling_footprint_indx[season_indx]] = desired - self.survey_features['N_obs_%i' % season].feature[self.rolling_footprint_indx][season_indx]
 
         result[self.out_of_bounds_area] = self.out_of_bounds_val
         return result
